12d1260ce32dc8e1291f35b465769da759fc0954
Arjun Singh <arjunmehra20@gmail.com>
2021-06-14 23:58:09 +0530

    gitignore file

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './css/App.css';
 import List from './component/list';
 
 function App() {
-  const alert1 = (add) =>{ alert(add)};
+  const alert1 = () =>{
+     //const textToAdd = document.getElementById("inputText").val();
+      alert("textToAdd")
+    };
     return (
       <div>
-        <input type="text"/>
-        <button onClick={() =>{alert1("gaurav")}}>Add</button>
+        <input htmlFor="dasd" id="inputText" type="text"/>
+        <button onClick={() =>{alert1()}}>Add</button>
         <List name="Tasks to do"/>
       </div>
     );
--- src/component/list.js
+++ src/component/list.js
@@ -2,14 +2,21 @@ import React, {Component} from 'react';
 import Row from './row';
 
 class List extends Component{
+    state = {
+        list : []
+    }
     render(){
         return (
         <div>
             <h1>{this.props.name}</h1>
             <ul>
-                <Row>1.</Row>
-                <Row>2.</Row>
-                <Row>3.</Row>
+                
+                { 
+                    
+                   this.state.list.length>0 ? this.state.list.map( (value)=>{
+                            return <Row>{value}</Row>
+                        }) : <Row>No task to do</Row>
+                }
             </ul>
         </div>) ;
     }
